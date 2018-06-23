5c0d2af9276d27e25ce0be750745acc1fdd23cbc
Tomas Patro <tomas.patro@gmail.com>
2018-06-23 21:12:23 +0200

    Added communication src files from Raspberry.

--- projects/communication/rpwifi/main.py
+++ projects/communication/rpwifi/main.py
@@ -1,4 +1,37 @@
 #!/usr/bin/python3
 
+import socket
+import sys
+
+import message_getting as mg
+
+BUFFER_SIZE = 128
+
+
+class Handler:
+    '''Main class which is evaluating incoming messages using finite state automata'''
+    def __init__(self, connection, client_address):
+        '''Constructor'''
+        self.connection = connection
+        self.client_address = client_address
+        self.listener = mg.Listener(self.connection)
+
+    def automata(self):
+        '''Function which is implementing automata for evaluation of messages'''    
+        while True:
+            packet = self.listener.get_message()
+            if packet == 'ERROR':
+                break
+
+
 def main():
-    pass
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('192.168.10.1', 10001)
+    print('Starting up on {}, port: {}'.format(*server_address))
+    sock.bind(server_address)
+    sock.listen(1)
+    while True:
+        connection, client_address = sock.accept()
+        h = Handler(connection, client_address)
+        h.automata()
+main()
---
+++ projects/communication/rpwifi/message_getting.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+BUFFER_SIZE = 128
+
+
+class Listener:
+    '''Class which is handling getting of new messages'''
+    def __init__(self, connection):
+        '''Constructor'''
+        self.connection = connection
+            
+    def get_message(self):
+        '''Function which is listening and waiting for a message'''
+        while True:
+            data = self.connection.recv(BUFFER_SIZE)
+            if data:
+                raw_data = data.decode('ascii')
+                print('Received:', raw_data)
+                self.connection.sendall('Hello!\n'.encode())
+                return raw_data
+            else:
+                return 'ERROR'
