deb5923d6055587a4c8c0e99aa0276967cdd8b16
Tomas Patro <tomas.patro@gmail.com>
2018-06-24 13:04:39 +0200

    Hot fix

--- projects/communication/rpwifi/main.py
+++ projects/communication/rpwifi/main.py
@@ -43,7 +43,7 @@ class Handler:
     def process_message(self, message):
         input_values = message.split('|')
         print(input_values, '<-')
-        if input_values[0] is not '1' and input_values[0] is not '3':
+        if (input_values[0] is not '1') and (input_values[0] is not '3'):
             #print(input_values[0], 'GOOD')
             return 'ERROR'
         
