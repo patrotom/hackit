0dcb2a2432a65cc457f7756bced53534f7a39d02
Tomas Patro <tomas.patro@gmail.com>
2018-06-24 12:43:13 +0200

    Hot fix

--- projects/communication/rpwifi/main.py
+++ projects/communication/rpwifi/main.py
@@ -44,7 +44,7 @@ class Handler:
         input_values = message.split('|')
         
         if input_values[0] is not 1 and input_values[0] is not 3:
-            print(input_values[0], 'GOOD')
+            #print(input_values[0], 'GOOD')
             return 'ERROR'
         
         for i in range (0, len(input_values), 1):
@@ -54,7 +54,7 @@ class Handler:
             tmp = 'in'
         else:
             tmp = 'out'
-
+        print(message)
         return self.control_unit.process(tmp, input_values[0], input_values[1], input_values[2], input_values[3])
 
     # def print_info(self, message):
