d1712269585e92fda78d0860e2980a91f0083f92
Tomas Patro <tomas.patro@gmail.com>
2018-06-24 13:28:07 +0200

    Hot fix.

--- projects/communication/rpwifi/message_getting.py
+++ projects/communication/rpwifi/message_getting.py
@@ -8,15 +8,24 @@ class Listener:
     def __init__(self, connection):
         '''Constructor'''
         self.connection = connection
-            
+        self.counter = 0
+
     def get_message(self):
         '''Function which is listening and waiting for a message'''
         while True:
             data = self.connection.recv(BUFFER_SIZE)
             if data:
                 raw_data = data.decode('ascii')
-                print('Received:', raw_data)
-                self.connection.sendall('Hello!\n'.encode())
+                print('R ========================== R')
+                print('TypeOfMessage-1|Temp|Hum|Pressure|In/Out')
+                print('TypeOfMessage-3|Fire|Gas|CO|In/Out')
+                print(raw_data)
+                print('R ========================== R')
+                self.counter += 1
+                if self.counter % 50 == 0:
+                    self.connection.sendall('1'.encode())
+                else:
+                    self.connection.sendall('2'.encode())
                 return raw_data
             else:
                 return 'ERROR'
