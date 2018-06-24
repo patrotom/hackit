817ea249f3c83e8f3e05ef6f3c2d78eb4b76476c
Tomas Patro <tomas.patro@gmail.com>
2018-06-24 10:16:35 +0200

    TCP to UDP.

--- projects/communication/rpwifi/message_getting.py
+++ projects/communication/rpwifi/message_getting.py
@@ -6,18 +6,18 @@ BUFFER_SIZE = 128
 
 class Listener:
     '''Class which is handling getting of new messages'''
-    def __init__(self, connection):
+    def __init__(self, sock):
         '''Constructor'''
-        self.connection = connection
+        self.sock = sock
             
     def get_message(self):
         '''Function which is listening and waiting for a message'''
         while True:
-            data = self.connection.recv(BUFFER_SIZE)
+            data, address = self.sock.recvfrom(BUFFER_SIZE)
             if data:
                 raw_data = data.decode('ascii')
                 print('Received:', raw_data)
-                self.connection.sendall('Hello!\n'.encode())
+                self.sock.sendto('Hello!\n'.encode(), address)
                 return raw_data
             else:
                 return 'ERROR'
